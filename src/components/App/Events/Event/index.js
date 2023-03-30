3256ca3cdecc96aeccc9cf4609ddf72737b0deeb
AntoineDeZotti <antoine.dezotti01@gmail.com>
2023-03-30 17:09:13 +0200

    footer done but need to fix image paths and started to work on Events & Event components

---
+++ src/components/App/Events/Event/index.js
@@ -0,0 +1,36 @@
+// == Import
+import Logo from '/Users/Antoine/Documents/my-partner-outdoor-front/public/image/logoGreen.png';
+import Facebook from '/Users/Antoine/Documents/my-partner-outdoor-front/public/image/facebook.png';
+import Instagram from '/Users/Antoine/Documents/my-partner-outdoor-front/public/image/instagram.png';
+import Twitter from '/Users/Antoine/Documents/my-partner-outdoor-front/public/image/twitter.png';
+import kayak from '/Users/Antoine/Documents/my-partner-outdoor-front/public/image/kayak.jpg';
+import './styles.scss';
+
+// == Composant
+function Event() {
+  return (
+        <div className='event'> 
+          <img className='event__image' src={kayak} alt='event' />
+          <div className='event__detail'>
+            <div>
+              <h5>Kayak dans le verdon</h5>
+                <div>
+                  <div>
+                    <img></img>
+                    <p>Lac du Salagou, 35070 Salga</p>
+                  </div>
+                  <div>
+                    <img></img>
+                    <p>15 participants maximum</p>
+                  </div>
+                </div>
+            </div>
+           </div>
+        </div>
+      
+    
+  );
+}
+
+// == Export
+export default Event;
