0e43c85e5b82b1c512d1951bbd8e23772494dbe5
AntoineDeZotti <antoine.dezotti01@gmail.com>
2023-03-30 17:09:13 +0200

    footer done but need to fix image paths and started to work on Events & Event components

--- src/components/App/Footer/index.js
+++ src/components/App/Footer/index.js
@@ -1,13 +1,11 @@
 // == Import
 
-//Chemin Image à corriger 
-
 // Import Image & Style
-import Logo from '/Users/Antoine/Documents/my-partner-outdoor-front/public/image/logoGreen.png';
-import Facebook from '/Users/Antoine/Documents/my-partner-outdoor-front/public/image/facebook.png';
-import Instagram from '/Users/Antoine/Documents/my-partner-outdoor-front/public/image/instagram.png';
-import Twitter from '/Users/Antoine/Documents/my-partner-outdoor-front/public/image/twitter.png';
-import Pinterest from '/Users/Antoine/Documents/my-partner-outdoor-front/public/image/pinterest.png';
+import Logo from 'src/assets/resource/logoGreen.png';
+import Facebook from 'src/assets/resource/facebook.png';
+import Instagram from 'src/assets/resource/instagram.png';
+import Twitter from 'src/assets/resource/twitter.png';
+import Pinterest from 'src/assets/resource/pinterest.png';
 import './styles.scss';
 
 // == Composant
