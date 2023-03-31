fdfc3da44b7df98ed11624f18286e8ba3ae41578
Baptiste Debree <funky_jacks@hotmail.com>
2023-03-31 09:48:47 +0200

    create header nav

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,6 +7,7 @@ import { Header } from '../Header';
 import { NotFound } from '../../pages/NotFound';
 import { Landing } from '../../pages/Landing';
 import { EventPage } from '../../pages/EventPage';
+import Footer from '../Footer';
 import Home from '../../pages/Home';
 import CreateEvent from '../../pages/CreateEvent';
 
