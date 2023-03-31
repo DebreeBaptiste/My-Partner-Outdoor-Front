50e4f02bfa85bfc396ca765ba8f5f6019cc3c7a9
Baptiste Debree <funky_jacks@hotmail.com>
2023-03-31 09:48:47 +0200

    create header nav

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,13 @@
-// == Import
+/* Tools */
+
+/* Component */
+import { Route, Routes } from 'react-router-dom';
+import { Header } from '../Header';
+import { Landing } from '../../pages/Landing';
 import { NotFound } from '../../pages/NotFound';
 import Home from '../../pages/Home';
+
+/* Style */
 import './styles.css';
 
 // == Composant
@@ -8,7 +15,12 @@ function App() {
   return (
     <div className="app">
       <h1>Composant : App</h1>
-      <Home /> 
+      <Home />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
