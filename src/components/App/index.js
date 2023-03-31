cee5f57a9955a70b43e132466bdd62ee3e475caf
Baptiste Debree <funky_jacks@hotmail.com>
2023-03-31 09:48:47 +0200

    create header nav

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,10 +4,18 @@
 import { Route, Routes } from 'react-router-dom';
 import { Header } from '../Header';
 import { Landing } from '../../pages/Landing';
+/* Tools */
+
+/* Component */
+import { Route, Routes } from 'react-router-dom';
+import { Header } from '../Header';
+import { Landing } from '../../pages/Landing';
 import { NotFound } from '../../pages/NotFound';
 
 /* Style */
 import Home from '../../pages/Home';
+
+/* Style */
 import './styles.css';
 
 // == Composant
@@ -21,6 +29,8 @@ function App() {
       </Routes>
       <h1>Composant : App</h1>
       <Home />
+
+
     </div>
   );
 }
