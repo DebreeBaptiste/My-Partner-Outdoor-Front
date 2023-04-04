4e5288213fb9d919cf126169d6eec8ad11faf4d2
Baptiste Debree <funky_jacks@hotmail.com>
2023-04-04 19:30:54 +0200

    create page event, tab about and chat

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,9 +4,10 @@ import React from 'react';
 /* Component */
 import { Route, Routes } from 'react-router-dom';
 import { Header } from '../Header';
-
 import { NotFound } from '../../pages/NotFound';
 import Footer from '../Footer';
+import { Landing } from '../../pages/Landing';
+import { EventPage } from '../../pages/EventPage';
 import Home from '../../pages/Home';
 import CreateEvent from '../../pages/CreateEvent';
 
@@ -23,6 +24,9 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/event/:id/chat" element={<EventPage />} />
+        <Route path="/event/:id/participants" element={<EventPage />} />
+        <Route path="/event/:id/" element={<EventPage />} />
         <Route path="/createevent" element={<CreateEvent />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
