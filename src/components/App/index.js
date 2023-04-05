9b297225aa4454bc0b7f0d0e782f8005aa3694e6
Baptiste Debree <funky_jacks@hotmail.com>
2023-04-05 17:51:20 +0200

    fix, fix css bug and warning on console after merged the two front branch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -16,10 +16,7 @@ import ForgetPassword from '../ForgetPassword';
 import { ModalLogin } from '../ModalLogin';
 
 /* Style */
-import './styles.css';
-import ForgetPassword from '../ForgetPassword';
 import './styles.scss';
-import { ModalLogin } from '../ModalLogin';
 
 // == Composant
 function App() {
