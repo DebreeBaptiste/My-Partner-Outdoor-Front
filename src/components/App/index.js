82b1497013d3553d427969fd17f2999fb4d41c9a
Baptiste Debree <funky_jacks@hotmail.com>
2023-04-06 08:51:37 +0200

    add hover effect for all button and body scroll lock when modal is open

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,7 +8,7 @@ import { NotFound } from '../../pages/NotFound';
 import Footer from '../Footer';
 import { Landing } from '../../pages/Landing';
 import { EventPage } from '../../pages/EventPage';
-import Footer from '../Footer';
+
 import Home from '../../pages/Home';
 import CreateEvent from '../../pages/CreateEvent';
 import ForgetPassword from '../ForgetPassword';
