1318a667d03104e7ce228168710f579a2c4734cd
Baptiste Debree <funky_jacks@hotmail.com>
2023-04-19 21:01:54 +0200

    fix image size and add radius on image

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -4,12 +4,12 @@ import { useEffect } from 'react';
 import { TeamCard } from '../../components/TeamCard';
 
 /* Image */
-import teamPicture1 from 'src/assets/resource/fake-team.jpg';
-import seb from 'src/assets/resource/seb.jpg';
-import antoine from 'src/assets/resource/antoine.jpg';
-import soukina from 'src/assets/resource/soukina.jpg';
-import jerome from 'src/assets/resource/jerome.jpeg';
-import baptiste from 'src/assets/resource/baptiste.jpg'
+
+import seb from 'src/assets/resource/seb.webp';
+import antoine from 'src/assets/resource/antoine.webp';
+import soukina from 'src/assets/resource/soukina.webp';
+import jerome from 'src/assets/resource/jerome.webp';
+import baptiste from 'src/assets/resource/baptiste.webp'
 
 /* Style */
 import './styles.scss';
@@ -27,7 +27,7 @@ export const Team = () => {
         <span className="team-header-subtitle">Découvrez l'équipe passionnée de My Partner Outdoor</span>
       </header>
       <div className="team-content">
-        <TeamCard picture={jerome} name="Jérôme" role="Product owner" github="https://github.com/jeromeconton" linkedin="https://www.linkedin.com/in/jerome-conton/"/>
+        <TeamCard picture={jerome} name="Jérôme" role="Product owner" github="https://github.com/jeromeconton" linkedin="https://www.linkedin.com/in/jerome-conton/" />
         <TeamCard picture={soukina} name="Soukina" role="Git master" github="https://github.com/Soukina-NOUNOU" linkedin="https://www.linkedin.com/in/soukina-nounou/" />
         <TeamCard picture={seb} name="Sébastien" role="Référant Technique" github="https://github.com/SebastienMercier91" linkedin="https://www.linkedin.com/in/sebastien-mercier-dev/" />
         <TeamCard picture={antoine} name="Antoine" role="Scrum master" github="https://github.com/AntoineDeZotti" linkedin="https://www.linkedin.com/in/antoine-de-zotti/" />
