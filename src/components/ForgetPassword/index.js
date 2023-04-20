08a49b1f8166bc13b09a5c95b2f451b089751347
Baptiste Debree <funky_jacks@hotmail.com>
2023-04-20 19:31:52 +0200

    fix some css one team page and forgettenpassword page

--- src/components/ForgetPassword/index.js
+++ src/components/ForgetPassword/index.js
@@ -5,17 +5,29 @@ import './styles.scss';
 // == Composant
 function ForgetPassword() {
   return (
-    <div className='forgetpassword'>
-      <h1 className='forgetpassword__title'>Mot de passe oublié</h1>
-      <form className='forgetpassword__form'>
-        <label className='forgetpassword__form__text'>Écrivez votre mail afin de générer un nouveau mot de passe</label>
-        <input className='forgetpassword__form__input' type="email" name="email" placeholder="Adresse email" />
-        <div className='forgetpassword__form__button'>
-          <button className='forgetpassword__form__button__cancel' type="button"><Link to="/">Annuler</Link></button>
-          <button className='forgetpassword__form__button__submit' type="submit">Envoyer</button>
-        </div>
-      </form>
-    </div>
+    <section className='forgetpassword-page'>
+
+      <div className='forgetpassword'>
+        <h1 className='forgetpassword__title'>Mot de passe oublié</h1>
+        <form className='forgetpassword__form'>
+          <p className='forgetpassword__form__text'>Remplissez les deux champs afin de réinitialiser votre mot de passe</p>
+          <p className='forgetpassword__form__text'>Saisissez un mot de passe en respectant :</p>
+          <p className='forgetpassword__form__text'>
+            <span>Minimum: 1 majuscule, </span>
+            <span>1 minuscule, </span>
+            <span>1 chiffre, </span>
+            <span>1 caractère spécial, </span>
+            <span>8 caractères.</span>
+          </p>
+          <input className='forgetpassword__form__input' type="password" name="password" placeholder="Votre mot de passe" />
+          <input className='forgetpassword__form__input' type="password" name="password_repeat" placeholder="Confirmez votre mot de passe" />
+          <div className='forgetpassword__form__button'>
+            <button className='forgetpassword__form__button__cancel' type="button"><Link to="/">Annuler</Link></button>
+            <button className='forgetpassword__form__button__submit' type="submit">Envoyer</button>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 }
 
